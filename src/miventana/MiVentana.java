1e81e2cccb03bda309422a86fe30c006191869e5
rodrigo govea <rodry.gm.isc@live.com.mx>
2020-03-10 08:01:27 -0600

    delete comments on main code

--- src/miventana/MiVentana.java
+++ src/miventana/MiVentana.java
@@ -3,7 +3,6 @@ public class MiVentana
 {
     public static void main(String[] args) 
     {
-        // TODO code application logic here
         laVentana myWindow = new laVentana();
         myWindow.inicializarComponentes();
         myWindow.setVisible(true);
