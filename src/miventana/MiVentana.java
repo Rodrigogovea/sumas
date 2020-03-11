a987c6e99ac4f48066008ced847aae25467824df
rodrigo govea <rodry.gm.isc@live.com.mx>
2020-03-11 11:27:02 -0600

    se agregaron dos líneas nuevas

--- src/miventana/MiVentana.java
+++ src/miventana/MiVentana.java
@@ -6,5 +6,7 @@ public class MiVentana
         laVentana myWindow = new laVentana();
         myWindow.inicializarComponentes();
         myWindow.setVisible(true);
+        
+        //esto es una línea de código nueva.
     }
 }
